ead8de8a4b8705f7c164a69ba6467f89dc40c4d0
Khine Wai <khine.wai@uconn.edu>
2019-11-10 01:55:40 -0500

    submit

---
+++ game.js
@@ -0,0 +1,22 @@
+var targetNubmer = 65;
+$("#guesstargetnumber").text(targetNumber)
+
+var wins = 0;
+var losses = 0;
+var totalscore = 0;
+
+var wins = $("#wins");
+var losses = $("#losses");
+var totalscore = $("#totalscore");
+
+wins.text("wins: "+ wins);
+losses.text("losses: " + losses);
+totalscore.text("Total Score: " + totalscore)
+
+var numberOptions = [20, 15, 10, 5]
+
+for (var i = 0; i <numberOptions.lengthl i++) { 
+
+$(".crystal1").on("click", function()
+
+
