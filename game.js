9354a550ec83dd4d218a6bf1565472b36d81610a
Khine Wai <khine.wai@uconn.edu>
2019-11-11 18:17:54 -0500

    submit3

--- game.js
+++ game.js
@@ -1,45 +1,88 @@
-var targetNubmer = 65;
-$("#guesstargetnumber").text(targetNumber)
+$(document).ready(function(){
+
+
 
 var wins = 0;
 var losses = 0;
 var totalscore = 0;
 
-var wins = $("#wins");
-var losses = $("#losses");
-var totalscore = $("#totalscore");
+// var winsdiv = $("#wins");
+// var lossesdiv = $("#losses");
+// var totalscore = $("#totalscore");
 
-wins.text("wins: "+ wins);
-losses.text("losses: " + losses);
 
-function wins() {
-    $("wins").text("Winner!");
+var randomnumber = Math.floor((Math.random() * 100) + 19);
+$("#guesstargetnumber").text(randomnumber);
+
+    $('#Crystal1').click(function(){
+        var Crystal1 = Math.floor((Math.random() * 12) + 1);
+        totalscore = totalscore + Crystal1;
+        $("#totalscore").text("totalscores: " + totalscore);
+        if (totalscore === randomnumber) { winscalc();
+        }
+        else if (totalscore > randomnumber) { lossescalc();
+        }
+        console.log (crystal1)
+    });
+    $('#Crystal2').click(function(){
+        var Crystal2 = Math.floor((Math.random() * 12) + 1);
+        totalscore = totalscore + Crystal2;
+        $("#totalscore").text("totalscores: " + totalscore);
+        if (totalscore === randomnumber) { winscalc();
+        }
+        else if (totalscore > randomnumber) { lossescalc();
+        }
+        console.log (crystal2)
+
+    });
+    $('#Crystal3').click(function(){
+        var Crystal3 = Math.floor((Math.random() * 12) + 1);
+        totalscore = totalscore + Crystal3;
+        $("#totalscore").text("totalscores: " + totalscore);
+        if (totalscore === randomnumber) { winscalc();
+        }
+        else if (totalscore > randomnumber) { lossescalc();
+        }
+        console.log (crystal3)
+
+
+    });
+    $('#Crystal4').click(function(){
+        var Crystal4 = Math.floor((Math.random() * 12) + 1);
+        totalscore = totalscore + Crystal4;
+        $("#totalscore").text("totalscores: " + totalscore);
+        if (totalscore === randomnumber) { winscalc();
+        }
+        else if (totalscore > randomnumber) { lossescalc();
+        }
+        console.log (crystal4)
+
+    });
+
+
+function winscalc(){
+
     wins++;
-    $("#wins").text(wins);
+    $("#wins").text("wins: " + wins);
+    reset ()
+
 }
 
-function losses() {
-    $("losses").text("loser!");
+function lossescalc(){
+
     losses++;
-    $("#losses").text(losses);
+    $("#losses").text("losses: " + losses);
+    reset ()
 }
 
-var numberOptions = [6, 12, 10, 1]
+function reset(){
 
-for (var i = 0; i <numberOptions.length; i++) { 
+    totalscore = 0
+    randomnumber = Math.floor((Math.random() * 100) + 19);
+    $("#guesstargetnumber").text(randomnumber);
+    $("#totalscore").text("totalscores: " + totalscore);
 
-$(".crystal").on("click", function(){
-
-var crystalValue = ($(this).attr("data-craystalvalue"));
-totalscore += crystalValue;
-
-if (totalscore === targetNubmer) {
-
-totalscore.text("Total Score: " + totalscore)
-   
 
 }
 
-}
-
-
+})
